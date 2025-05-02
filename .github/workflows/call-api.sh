e663cb5d5e4668bb920cc5c79295f3e18907e1e5
Shahid Azim <shahidazim@hotmail.com>
2025-05-02 12:21:32 +0100

    migrated inline script to a separate file

---
+++ .github/workflows/call-api.sh
@@ -0,0 +1,18 @@
+# Find all .md files and convert to a JSON array
+JSON_ARRAY=$(find $BASE_FOLDER \( -path "./vendor" -o -path "./_site" \) -prune -o -name "*.md" -print | sed 's|^\./||' | while read -r file; do
+  echo "\"$file\""
+done | paste -sd "," -)
+
+# Wrap in payload and square brackets to form a JSON array
+JSON_BODY="{ baseUrl: \"$BASE_URL\", documentPaths: [$JSON_ARRAY] }"
+
+# Optional: Echo the JSON to check structure
+echo "$JSON_BODY"
+
+# Send JSON array to Azure Function
+curl -X POST "$API_URL" \
+  -H "Content-Type: application/json" \
+  -H "X-Api-Key: $API_KEY" \
+  -d "$JSON_BODY"
+
+echo "API call completed."
